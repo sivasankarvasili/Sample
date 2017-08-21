54c9f3704176f31758154dc53c81713a11a35480
sivasankar <sivasankar.spring@gmail.com>
2017-08-21 10:25:50 +0530

    commetns 5

--- src/com/ibm/controller/PlayingGame.java
+++ src/com/ibm/controller/PlayingGame.java
@@ -74,6 +74,7 @@ public class PlayingGame {
 	            	iterator.remove();
 	            	//System.out.println("Array Size"+names.size());
 	            }
+	        	//displays scores
 	        	System.out.println(key + " -------- "+ value);
 	        }
 	
